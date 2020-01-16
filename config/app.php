38ea5c1ba61557c490bc840bf5f5413d3b45b0b6
Jason Klein <jason.klein@logicforte.com>
2020-01-16 16:54:44 -0600

    Laravel Health
    composer require pragmarx/health
    vi config/app.php
    php artisan vendor:publish --provider="PragmaRX\Health\ServiceProvider"

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        PragmaRX\Health\ServiceProvider::class,
 
         /*
          * Application Service Providers...
